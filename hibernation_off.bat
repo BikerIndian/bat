f54fc9e81c21b4052c4dca7afcd6c6664934b8e0
Vladimir Svishch (IndianBiker) <5693031@gmail.com>
2017-07-14 11:59:08 +0300

    Отключение гибернации в Windows
    Включенная гибернация ведет к увеличению файла hiberfil.sys  в Windows

---
+++ hibernation_off.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:: ***********************************************
+::  Отключение гибернации в Windows 
+::  Date: 24.06.2009
+::  v1.00
+::  Authors:
+::   Vladimir Svishch (IndianBiker)  <mail:5693031@gmail.com>
+::   https://github.com/BikerIndian/bat
+:: ***********************************************
+powercfg -h off
+
